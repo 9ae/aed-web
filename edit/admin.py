ea5ba6496063fcadb0822140e6fe39e557b61ed5
Ari Qing <ari@aviator.home>
2013-11-24 22:52:04 -0500

    save

--- edit/admin.py
+++ edit/admin.py
@@ -4,7 +4,7 @@ Created on Nov 24, 2013
 @author: ari
 '''
 from django.contrib import admin
-from models import Paradigm, Protocol,Interval,Event,Action, IntervalProperty,EventProperty,ActionProperty
+from models import *
 
 admin.site.register(Paradigm)
 admin.site.register(Protocol)
@@ -14,3 +14,6 @@ admin.site.register(Action)
 admin.site.register(IntervalProperty)
 admin.site.register(EventProperty)
 admin.site.register(ActionProperty)
+admin.site.register(IntervalBegin)
+admin.site.register(IntervalEnd)
+admin.site.register(IntervalAction)
--- edit/models.py
+++ edit/models.py
@@ -28,6 +28,9 @@ class Paradigm(models.Model):
 	name = models.CharField(max_length=100)
 	file_location = models.FilePathField(path='/Users/ari/slytherin/aed-web/paradigms',match='^[a-zA-Z0-9]*.py')
 	created_on=models.DateTimeField(auto_now_add=True)
+	
+	def __unicode__(self):
+		return u'[%d] %s' % (self.id, self.name)
 
 class Protocol(models.Model):
 	paradigm = models.ForeignKey(Paradigm)
@@ -35,6 +38,9 @@ class Protocol(models.Model):
 	trial_duration = models.DecimalField(max_digits=8, decimal_places=3,default=0.0)
 	created_on = models.DateTimeField(auto_now_add=True)
 	modified_on = models.DateTimeField(auto_now=True)
+	
+	def __unicode__(self):
+		return u'[%d] %s<%s' % (self.id, self.name, self.paradigm.name)
 
 class Interval(models.Model):
 	protocol = models.ForeignKey(Protocol)
@@ -44,20 +50,55 @@ class Interval(models.Model):
 	name = models.CharField(max_length=100)
 	# put actions and events
 
+	def __unicode__(self):
+		return u'[%d] %s(%s) < %s' % (self.id, self.name, self.type, self.protocol)
+
 class IntervalProperty(ParadigmProperty):
 	interval = models.ForeignKey(Interval)
+	
+	def __unicode__(self):
+		return u'%s[%d].%s'%(self.interval.name,self.interval.id,self.prop_name)
 
 class Action(models.Model):
-	protocol = models.ForeignKey(Protocol)
+	paradigm = models.ForeignKey(Paradigm)
 	type = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return u'[%d] %s < %s' % (self.id, self.type, self.paradigm.name)
 
 class ActionProperty(ParadigmProperty):
 	action = models.ForeignKey(Action)
+
+	def __unicode__(self):
+		return u'%s[%d].%s'%(self.action.type,self.action.id,self.prop_name)
 
 class Event(models.Model):
 	protocol = models.ForeignKey(Protocol)
 	type = models.CharField(max_length=100)
 	name = models.CharField(max_length=100)
 
+	def __unicode__(self):
+		return u'[%d] %s(%s) < %s' % (self.id, self.name, self.type, self.protocol.name)
+
 class EventProperty(ParadigmProperty):
 	event = models.ForeignKey(Event)
+
+	def __unicode__(self):
+		return u'%s[%d].%s'%(self.event.name, self.event.id,self.prop_name)
+	
+class EventsChain(models.Model):
+	order = models.PositiveSmallIntegerField()
+	event = models.ForeignKey(Event)
+	
+	class Meta:
+		abstract = True
+
+class IntervalBegin(EventsChain):
+	interval = models.ForeignKey(Interval)
+
+class IntervalEnd(EventsChain):
+	interval = models.ForeignKey(Interval)
+
+class IntervalAction(EventsChain):
+	interval = models.ForeignKey(Interval)
+	action = models.ForeignKey(Action)
